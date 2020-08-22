3f24d8de469793bfc60e40e9952d1171b78d0a50
James <jimhenegan@gmail.com>
2020-08-21 21:57:42 -0500

    improved lesson scraper --- made it more robust

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,80 @@
+import re
+
+def clear_terminal():
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
+
+def make_intro_text_1():        
+    print("What should the name of the CSV file be?")
+    print("For example, type 'file' to create 'file.csv'")
+    print("")
+
+def get_text(file_name):
+    text = ""
+    with open(file_name,'r') as file:
+        text = file.read()
+    return text
+
+
+##############################
+# Helpers for scrape_lessons
+##############################
+
+def make_intro_text_0():
+    print("-------------------------")
+    print("Udemy Course Info Scraper")
+    print("-------------------------")
+    print("")
+    print("We will convert the contents of 'sample_lessons.txt' to a CSV file.")
+
+def begins_with_digit_and_dot(txt):
+    return re.search("^\d+[.]", txt)
+
+def ends_with_digits_and_min(txt):
+    return re.search("\d+min$", txt)
+
+def get_titles_and_lengths(text):
+
+    split_string = text.split("\n")
+
+    lesson_names = []
+    lengths = []
+
+    begin_desc_line_num = 0
+    end_desc_line_num = 0
+
+    for j in range(len(split_string)):
+        if begins_with_digit_and_dot(split_string[j]):
+            begin_desc_line_num = j
+        elif ends_with_digits_and_min(split_string[j]):
+            end_desc_line_num = j - 1
+            time = split_string[j].split("min")[0]
+
+            if begin_desc_line_num == end_desc_line_num:
+                description = split_string[begin_desc_line_num]
+            else:
+                description = ""
+                for i in range(begin_desc_line_num, end_desc_line_num + 1):
+                    description += split_string[i] + " "
+            lesson_names.append(description)
+            lengths.append(time)
+    
+    return(lesson_names, lengths)
+
+
+##############################
+# Helpers for scrape_sections
+##############################
+
+def make_intro_text_0_sections():
+    print("-------------------------")
+    print("Udemy Course Info Scraper")
+    print("-------------------------")
+    print("")
+    print("We will convert the contents of 'sample_sections.txt' to a CSV file.")    
+
+
 def get_hours(your_list):
     if len(your_list) == 2:
         return your_list[0]
@@ -22,60 +99,11 @@ def clean_time(df):
 
     return df        
 
-def clear_terminal():
-    print(chr(27)+'[2j')
-    print('\033c')
-    print('\x1bc')
-
-def make_intro_text_0():
-    print("-------------------------")
-    print("Udemy Course Info Scraper")
-    print("-------------------------")
-    print("")
-    print("We will convert the contents of 'sample_lessons.txt' to a CSV file.")
-
-def make_intro_text_0_sections():
-    print("-------------------------")
-    print("Udemy Course Info Scraper")
-    print("-------------------------")
-    print("")
-    print("We will convert the contents of 'sample_sections.txt' to a CSV file.")    
-
-def make_intro_text_1():        
-    print("What should the name of the CSV file be?")
-    print("For example, type 'file' to create 'file.csv'")
-    print("")
-
 def make_intro_text_2():    
     print("What is the name of the section?")
     print("Example: 11. Microservice Architectures")
     print("")
 
-def get_text(file_name):
-
-    text = ""
-
-    with open(file_name,'r') as file:
-        text = file.read()
-
-    return text
-
-def get_titles_and_lengths(text):
-
-    split_string = text.split("\n")
-
-    section_names = []
-    lengths = []
-
-    for j in range(len(split_string)):
-        try:
-            if split_string[j+1][-3:] == "min":
-                section_names.append(split_string[j])
-                lengths.append(split_string[j+1][:-3])
-        except:
-            print("oops")
-
-    return(section_names, lengths)
 
 def get_section_titles_and_lengths(text):
 
--- app/scrape_lessons.py
+++ app/scrape_lessons.py
@@ -2,24 +2,50 @@ import pandas as pd
 from helpers import *
 import os
 
+###################################
+# Where is your text file located?
+###################################
 file_to_read = os.path.join("..", "txt", "sample_lessons.txt")
+
+###################################
+# Make introduction text
+###################################
 
 clear_terminal()
 make_intro_text_0()
 
+####################################
+# Ask for the name of output file
+####################################
+
 make_intro_text_1()
 file_to_save = input("Output file name:  ") + ".csv"
 file_to_save = os.path.join("..", "csv", file_to_save)
-
 clear_terminal()
+
+###############################################
+# Ask for the name of the Section in the course
+###############################################
+
 make_intro_text_2()
 section_name = input("Section Name:  ")
 
+################################################
+# Load in the text from the file
+################################################
+
 text_from_file = get_text(file_to_read) 
 
-(lesson_names,lengths) = get_titles_and_lengths(text_from_file)
+################################################
+# Extract relevant data from the text
+################################################
 
+(lesson_names,lengths) = get_titles_and_lengths(text_from_file)
 clear_terminal()
+
+#################################################
+# Make a dataframe out the data
+#################################################
 
 df = pd.DataFrame(
     list(zip(lesson_names, lengths)),
@@ -29,7 +55,10 @@ df = pd.DataFrame(
 df['Section'] = section_name
 df = df[['Section', 'Lesson', 'Time']]
 
-df.to_csv(file_to_save, index = False)
+##################################################
+# Save the Dataframe to a CSV file
+##################################################
 
+df.to_csv(file_to_save, index = False)
 
 print("File created!")
